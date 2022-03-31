c8b5fb65ad1bae970ef73d5a9479fa936e6084e1
gjakubik <gavito8800@gmail.com>
2022-03-31 17:15:54 -0400

    more testa

---
+++ hw5/testGetCustomerTotal.sql
@@ -0,0 +1,15 @@
+-- finds total of all transactions of a given customer
+declare
+    cid customer.cust_id%type := '&id';  -- the ampersand allows the user to input the value
+    cname customer.cust_name%type;
+    tot product.price%type;
+begin
+    cname := slpkg.get_cust_name(cid);
+    tot := slpkg.get_customer_total(cid);
+    if tot is null then
+        dbms_output.put_line('Can''t find customer');
+    else
+        dbms_output.put_line('The total of all  ' || cname || '''s purchases is ' || tot || '$');
+    end if;
+end;
+/
--- hw5/testGetTransactionTotal.sql
+++ hw5/testGetTransactionTotal.sql
@@ -1,13 +1,13 @@
 -- finds total of a given transaction
 declare
-  sid sale.sale_id%type := '&id';  -- the ampersand allows the user to input the value
-  tot product.price%type;
+    sid sale.sale_id%type := '&id';  -- the ampersand allows the user to input the value
+    tot product.price%type;
 begin
-  tot := slpkg.get_transaction_total(sid);
-  if tot is null then
-    dbms_output.put_line('Can''t find transaction');
-  else
-    dbms_output.put_line('The total for transaction' || sid || ' is ' || tot || '$');
-  end if;
+    tot := slpkg.get_transaction_total(sid);
+    if tot is null then
+        dbms_output.put_line('Can''t find transaction');
+    else
+        dbms_output.put_line('The total for transaction ' || sid || ' is ' || tot || '$');
+    end if;
 end;
 /
